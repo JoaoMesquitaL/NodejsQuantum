e334a2b9c72a4d7c8dd4642a2a52900aef384514
JoaoMesquitaL <joao.pedroml@protonmail.com>
2025-07-29 21:10:38 -0300

    alterado o metodo get para retornar json

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
-  res.send('Olá Mundo!')
+  res.json({message: 'Olá Mundo!'})
 })
 
 app.listen(port, () => {
